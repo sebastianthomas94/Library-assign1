319d2307f045c51e60b7869bd17555125c10e275
Sebastianthomas94 <Sebastianthomas94@outlook.com>
2019-08-13 16:01:46 +0530

    13-08-2019

--- register.js
+++ register.js
@@ -7,13 +7,13 @@ $("#rgbtn").click(function(){
     var cpw = $("#password2").val();
     var regexUn = /^[a-zA-Z ]{1,32}$/;
     var regexPw = /^[a-zA-Z0-9]{1,32}$/;
-    if( (un.length!=0 && regexUn.test(un)) && (pw.length!=0 && regexPw.test(pw)) && pw == cpw )
+    if( (username.length!=0 && regexUn.test(un)) && (password1.length!=0 && regexPw.test(password1)) && password1 == password2 )
     {
         window.location.href="./index.html";
         
     }
     else
-    {   if(un.length == 0 || pw.length == 0)
+    {   if(username.length == 0 || password1.length == 0|| password2.length == 0)
         alert("Username and Password cannot be empty");
         if((!regexUn.test(un)) || (!regexPw.test(pw)) )
         alert("Please enter Username and Password in the correct format")
